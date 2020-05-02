0ae3c0a5558aaad660e7e41a60f87fb79d412f9e
SuprajaKamalapuram <59463956+SuprajaKamalapuram@users.noreply.github.com>
2020-05-02 18:32:25 +0530

    Add files via upload

---
+++ restaurant_service/src/main/java/com/spicy/kitchen/dao/RestaurantDao.java
@@ -0,0 +1,15 @@
+package com.spicy.kitchen.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.spicy.kitchen.model.Restaurant;
+
+@Repository
+public interface RestaurantDao extends CrudRepository<Restaurant , Integer>  {
+
+	
+	
+	
+	
+}
